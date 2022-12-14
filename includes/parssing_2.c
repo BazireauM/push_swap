ad64743c907beb8e548c35514591613654175fd1
Mathias Bazireau <mbazirea@k1r4p10.42mulhouse.fr>
2022-12-14 12:39:18 +0100

    save

--- includes/parssing_2.c
+++ includes/parssing_2.c
@@ -6,7 +6,7 @@
 /*   By: mbazirea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 10:24:48 by mbazirea          #+#    #+#             */
-/*   Updated: 2022/12/14 12:29:23 by mbazirea         ###   ########.fr       */
+/*   Updated: 2022/12/14 12:38:48 by mbazirea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,17 @@ int	put_number_in_a(t_stack *stack, char *argv, int *a_index)
 	stack->a[*a_index] = ft_atoi(argv);
 	return (0);
 }
-/*
-void	put_string_number_in_a(t_stack stack, char *argv, int *a_index)
+
+int	put_string_number_in_a(t_stack stack, char *argv, int *a_index)
 {
+	int	i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (argv[i] = ' ' && argv[i - 1] && ft_isdigit(argv[i - 1] == 1))
+		{
+		}
+		i++;
+	}
 }
-*/
